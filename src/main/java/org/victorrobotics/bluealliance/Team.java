2bc955d761a034c7b3cf1ddf974620825f99a49e
MathNerd28 <xander.bhalla@gmail.com>
2023-10-24 11:51:08 -0400

    Refactor Team to class instead of record

--- src/main/java/org/victorrobotics/bluealliance/Team.java
+++ src/main/java/org/victorrobotics/bluealliance/Team.java
@@ -1,24 +1,56 @@
 package org.victorrobotics.bluealliance;
 
 import java.util.List;
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record Team(@JsonProperty("key") String id,
-                   @JsonProperty("team_number") int number,
-                   @JsonProperty("nickname") String name,
-                   @JsonProperty("name") String fullName,
-                   @JsonProperty("school_name") String school,
-                   @JsonProperty("city") String city,
-                   @JsonProperty("state_prov") String province,
-                   @JsonProperty("country") String country,
-                   @JsonProperty("postal_code") String postalCode,
-                   @JsonProperty("website") String website,
-                   @JsonProperty("rookie_year") int rookieYear) {
-  public record Robot(@JsonProperty("year") int year,
-                      @JsonProperty("robot_name") String robotName,
-                      @JsonProperty("key") String robotKey,
-                      @JsonProperty("team_key") String teamKey) {
+public class Team {
+  @JsonProperty
+  private String key;
+
+  @JsonProperty("team_number")
+  private int number;
+
+  @JsonProperty("nickname")
+  private String name;
+
+  @JsonProperty("name")
+  private String fullName;
+
+  @JsonProperty("school_name")
+  private String school;
+
+  @JsonProperty
+  private String city;
+
+  @JsonProperty("state_prov")
+  private String province;
+
+  @JsonProperty
+  private String country;
+
+  @JsonProperty("postal_code")
+  private String postalCode;
+
+  @JsonProperty
+  private String website;
+
+  @JsonProperty("rookie_year")
+  private int rookieYear;
+
+  public static class Robot {
+    @JsonProperty
+    private int year;
+
+    @JsonProperty("robot_name")
+    private String name;
+
+    @JsonProperty
+    private String key;
+
+    @JsonProperty("team_key")
+    private String teamKey;
 
     public static Endpoint<List<Robot>> endpointForTeam(String teamKey) {
       return Endpoint.forList("/team/" + teamKey + "/robots", Robot.class);
@@ -35,5 +67,97 @@ public record Team(@JsonProperty("key") String id,
 
   public static Endpoint<List<Team>> endpointForYear(int year, int page) {
     return Endpoint.forList("/teams/" + year + "/" + page, Team.class);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("Team [key=")
+           .append(key)
+           .append(", number=")
+           .append(number)
+           .append(", name=")
+           .append(name)
+           .append(", fullName=")
+           .append(fullName)
+           .append(", school=")
+           .append(school)
+           .append(", city=")
+           .append(city)
+           .append(", province=")
+           .append(province)
+           .append(", country=")
+           .append(country)
+           .append(", postalCode=")
+           .append(postalCode)
+           .append(", website=")
+           .append(website)
+           .append(", rookieYear=")
+           .append(rookieYear)
+           .append("]");
+    return builder.toString();
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public String getSchool() {
+    return school;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public String getProvince() {
+    return province;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public String getPostalCode() {
+    return postalCode;
+  }
+
+  public String getWebsite() {
+    return website;
+  }
+
+  public int getRookieYear() {
+    return rookieYear;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(key, number, name, fullName, school, city, province, country, postalCode,
+                        website, rookieYear);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (!(obj instanceof Team)) return false;
+    Team other = (Team) obj;
+    return Objects.equals(key, other.key) && number == other.number
+        && Objects.equals(name, other.name) && Objects.equals(fullName, other.fullName)
+        && Objects.equals(school, other.school) && Objects.equals(city, other.city)
+        && Objects.equals(province, other.province) && Objects.equals(country, other.country)
+        && Objects.equals(postalCode, other.postalCode) && Objects.equals(website, other.website)
+        && rookieYear == other.rookieYear;
   }
 }
