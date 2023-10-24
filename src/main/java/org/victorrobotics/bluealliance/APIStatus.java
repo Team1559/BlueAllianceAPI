ffa4bb5e5f7a4dac95c9932b74a25c46dee5d17a
MathNerd28 <xander.bhalla@gmail.com>
2023-10-24 15:16:53 -0400

    Refactor APIStatus to class instead of record

--- src/main/java/org/victorrobotics/bluealliance/APIStatus.java
+++ src/main/java/org/victorrobotics/bluealliance/APIStatus.java
@@ -1,17 +1,134 @@
 package org.victorrobotics.bluealliance;
 
+import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record APIStatus(@JsonProperty("current_season") int currentSeason,
-                        @JsonProperty("max_season") int maxSeason,
-                        @JsonProperty("is_datafeed_down") boolean isDatafeedDown,
-                        @JsonProperty("down_events") Set<String> downEvents,
-                        @JsonProperty("ios") AppVersion iosVersion,
-                        @JsonProperty("android") AppVersion androidVersion) {
-  public record AppVersion(@JsonProperty("min_app_version") int minimum,
-                                  @JsonProperty("latest_app_version") int latest) {}
+public class APIStatus {
+  public static class AppVersion {
+    @JsonProperty("min_app_version")
+    private int minimum;
+
+    @JsonProperty("latest_app_version")
+    private int latest;
+
+    public AppVersion() {}
+
+    public int getMinimum() {
+      return minimum;
+    }
+
+    public int getLatest() {
+      return latest;
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder builder = new StringBuilder();
+      builder.append("AppVersion [minimum=")
+             .append(minimum)
+             .append(", latest=")
+             .append(latest)
+             .append("]");
+      return builder.toString();
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(minimum, latest);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) return true;
+      if (!(obj instanceof AppVersion)) return false;
+      AppVersion other = (AppVersion) obj;
+      return minimum == other.minimum && latest == other.latest;
+    }
+  }
+
+  @JsonProperty("current_season")
+  private int currentSeason;
+
+  @JsonProperty("max_season")
+  private int maxSeason;
+
+  @JsonProperty("is_datafeed_down")
+  private boolean isDatafeedDown;
+
+  @JsonProperty("down_events")
+  private Set<String> downEvents;
+
+  @JsonProperty("ios")
+  private AppVersion iosVersion;
+
+  @JsonProperty("android")
+  private AppVersion androidVersion;
+
+  public APIStatus() {}
+
+  public int getCurrentSeason() {
+    return currentSeason;
+  }
+
+  public int getMaxSeason() {
+    return maxSeason;
+  }
+
+  @JsonIgnore // grabbed by default during serialization
+  public boolean isDatafeedDown() {
+    return isDatafeedDown;
+  }
+
+  public Set<String> getDownEvents() {
+    return downEvents;
+  }
+
+  public AppVersion getIosVersion() {
+    return iosVersion;
+  }
+
+  public AppVersion getAndroidVersion() {
+    return androidVersion;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("APIStatus [currentSeason=")
+           .append(currentSeason)
+           .append(", maxSeason=")
+           .append(maxSeason)
+           .append(", isDatafeedDown=")
+           .append(isDatafeedDown)
+           .append(", downEvents=")
+           .append(downEvents)
+           .append(", iosVersion=")
+           .append(iosVersion)
+           .append(", androidVersion=")
+           .append(androidVersion)
+           .append("]");
+    return builder.toString();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(currentSeason, maxSeason, isDatafeedDown, downEvents, iosVersion,
+                        androidVersion);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (!(obj instanceof APIStatus)) return false;
+    APIStatus other = (APIStatus) obj;
+    return currentSeason == other.currentSeason && maxSeason == other.maxSeason
+        && isDatafeedDown == other.isDatafeedDown && Objects.equals(downEvents, other.downEvents)
+        && Objects.equals(iosVersion, other.iosVersion)
+        && Objects.equals(androidVersion, other.androidVersion);
+  }
 
   public static Endpoint<APIStatus> endpoint() {
     return Endpoint.forSingle("/status", APIStatus.class);
