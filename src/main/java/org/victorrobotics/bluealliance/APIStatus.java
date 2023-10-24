e9bc25db7213ebf29c9b8c51a92d89a9edc9f213
MathNerd28 <xander.bhalla@gmail.com>
2023-10-24 11:27:52 -0400

    Add AppVersion to APIStatus

--- src/main/java/org/victorrobotics/bluealliance/APIStatus.java
+++ src/main/java/org/victorrobotics/bluealliance/APIStatus.java
@@ -7,7 +7,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record APIStatus(@JsonProperty("current_season") int currentSeason,
                         @JsonProperty("max_season") int maxSeason,
                         @JsonProperty("is_datafeed_down") boolean isDatafeedDown,
-                        @JsonProperty("down_events") Set<String> downEvents) {
+                        @JsonProperty("down_events") Set<String> downEvents,
+                        @JsonProperty("ios") AppVersion iosVersion,
+                        @JsonProperty("android") AppVersion androidVersion) {
+  public record AppVersion(@JsonProperty("min_app_version") int minimum,
+                                  @JsonProperty("latest_app_version") int latest) {}
 
   public static Endpoint<APIStatus> endpoint() {
     return Endpoint.forSingle("/status", APIStatus.class);
